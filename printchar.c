11672a2fb6b365ed5656472d2de69d416a42ce25
zugo-m <99685288+zugo-m@users.noreply.github.com>
2022-04-19 23:54:41 +0100

    Create printchar.c

---
+++ printchar.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * printchar - outputs characters
+ *
+ * @list_args - list of arguments
+ *
+ * Return: int
+ */
+int printchar(va_list list_args)
+{
+  _putchar(va_arg(list_args, int));
+  return (1);
+}
