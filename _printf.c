88fc1bb36a0f58ae588639ce98512854611daf20
zugo-m <99685288+zugo-m@users.noreply.github.com>
2022-04-19 18:06:41 +0100

    Create _printf.c

---
+++ _printf.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *_printf - prints a string with specifiers.
+ *@format: the main string passed to the function.
+ *
+ *Return: number of total characters printed to stdout
+ */
+int _printf(const char *format, ...)
+{
+	int printed_chars;
+
+	conver_struct print_list[] = {
+		{"%", prints_percent},
+		{"d", prints_numbers},
+		{"i", prints_numbers},
+		{"c", prints_character},
+		{"s", prints_string},
+		{"R", prints_rot13},
+		{NULL, NULL}
+	};
+
+	va_list arg_list;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(arg_list, format);
+	printed_chars = selector(format, print_list, arg_list);
+	va_end(arg_list);
+
+	return (prined_chars);
+}
