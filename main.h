d023ef112a96524cebe6923b65e90c19b03b1d12
zugo-m <99685288+zugo-m@users.noreply.github.com>
2022-04-19 17:46:03 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,33 +1,44 @@
-#ifndef File_Main
-#define File_Main
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdarg.h>
-
-int _putchar(char c);
-int print_int(va_list arg);
-int print_unsigned(va_list arg);
-int _printf(const char *format, ...);
-int print_char(va_list arg);
-int print_str(va_list arg);
-int print_percent(void);
-void print_binary(unsigned int n, unsigned int* printed);
-int print_unsignedToBinary(va_list arg);
-int print_oct(va_list arg);
-int print_unsignedIntToHex(unsigned int num, char _case);
-int print_hex_base(va_list arg, char _case);
-int print_hex(va_list arg);
-int print_HEX(va_list arg);
-int print_STR (va_list arg);
+#include <stdlib.h>
+#include <limits.h>
+#include <unistd.h>
+#include <string.h>
 
 /**
- * struct identifierStruct - structure definition of a printTypeStruct
- * @indentifier: type
- * @printer: function to print
- **/
-typedef struct identifierStruct
+ * struct conv - a struct of a function pointer and a character
+ * @ch: a conversion specifier character
+ * @f: a function pointer
+ */
+
+typedef struct conv
 {
-	char *indentifier;
-	int (*printer)(va_list);
-} identifierStruct;
+	char ch;
+	int (*f)(va_list);
+} spec;
+
+char tstr(unsigned int n);
+void reve(char *buff);
+int printrevstr(va_list pargs);
+void rot13(char *s);
+int printrotstr(va_list pargs);
+void _itoa(long int v, char *s, int base);
+void _uitoa(unsigned long int v, char *s, unsigned int base);
+int printchar(va_list pargs);
+int printuns(va_list pargs);
+int printuoct(va_list pargs);
+int printubin(va_list pargs);
+int printuhex(va_list pargs);
+int printuhex_C(va_list pargs);
+int printaddr(va_list pargs);
+int (*get_func(char c))(va_list);
+int printstr(va_list pargs);
+int printstr_custom(va_list pargs);
+int _putchar(char c);
+int _printf(const char *format, ...);
+void print_str(char *str);
+int printint(va_list pargs);
 
 #endif
