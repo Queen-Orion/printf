4d38af5d5a5d34349aeaa8c6189688c2a3264325
zugo-m <99685288+zugo-m@users.noreply.github.com>
2022-04-19 18:36:29 +0100

    Create main.h

---
+++ main.h
@@ -0,0 +1,28 @@
+#ifndef _PRINT_F
+#define _PRINT_F
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * struct conversion - This struct content.
+ *@spec: specifier char
+ *@f: pointer to function in print list
+ */
+struct conversion
+{
+	char *spec;
+	int (*f)(va_list);
+};
+typedef struct conversion conver_struct;
+
+int _printf(const char *format, ...);
+int selector(const char *format, conver_struct print_list[], va_list arg_list);
+int put_char(char);
+int prints_character(va_list);
+int prints_string(va_list);
+int prints_percent(va_list);
+int prints_numbers(va_list);
+int prints_rot13(va_list);
+
+#endif
